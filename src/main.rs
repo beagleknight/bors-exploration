223a83f0b05c6e56fa5a851b83f83742d361b9e9
David Morcillo <david.morcillo@factorial.co>
2022-04-29 11:04:29 +0200

    Remove foo

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,10 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn foo() -> i32 {
-    return 4;
-}
-
 #[cfg(test)]
 mod tests {
-    use crate::foo;
-
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, foo());
+        assert_eq!(2 + 2, 4);
     }
 }
