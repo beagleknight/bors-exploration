a9b2e4a9ef88bebc1dbd125eff7ae5d0fcbd0c44
David Morcillo <david.morcillo@factorial.co>
2022-04-29 11:12:40 +0200

    more changes

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ fn main() {
     println!("Hello, world!");
 }
 
+mod foo {
+    pub fn get_value() -> i32 {
+        return 10;
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::foo;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(10, foo::get_value());
     }
 }
