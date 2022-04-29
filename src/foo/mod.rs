88549d06c3016475702e6f22a99aebb053a718b3
David Morcillo <david.morcillo@factorial.co>
2022-04-29 11:16:51 +0200

    Refactor modules

---
+++ src/foo/mod.rs
@@ -0,0 +1,5 @@
+pub mod foo {
+    pub fn get_value() -> i32 {
+        return 12;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
+pub mod foo;
+
 fn main() {
     println!("Hello, world!");
 }
 
-mod foo {
-    pub fn get_value() -> i32 {
-        return 10;
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::foo;
+    use super::foo::foo;
 
     #[test]
     fn it_works() {
-        assert_eq!(10, foo::get_value());
+        assert_eq!(12, foo::get_value());
     }
 }
