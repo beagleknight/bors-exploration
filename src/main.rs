63a2b6397da9f872c1b6ca054ab64b8862829969
David Morcillo <david.morcillo@factorial.co>
2022-04-29 11:01:45 +0200

    Some changes

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn foo() -> i32 {
+    return 4;
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::foo;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(2 + 2, foo());
     }
 }
